1d56dcd34e0cf5b8c64aec6a8668aac405dd4bd6
Max Heilmann <support@matix-media.net>
2021-04-15 17:59:13 +0200

    Create CommandHandler.java

---
+++ CommandHandler/CommandHandler.java
@@ -0,0 +1,57 @@
+package net.matixmedia.utils.command;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import java.util.Arrays;
+import java.util.Map;
+
+public class CommandHandler implements CommandExecutor {
+
+    private String name;
+    private String label;
+    private String prefix;
+    private ICommand baseCommand;
+    private Map<String, ICommand> commands;
+    private Map<String, String> permissions;
+
+    public CommandHandler(String name, String label, String prefix) {
+        this.name = name;
+        this.label = label;
+        this.prefix = prefix;
+    }
+
+    public void addCommand(String label, ICommand command, String permission) {
+        commands.put(label, command);
+        permissions.put(label, permission);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
+        if (args.length == 0 && baseCommand == null) {
+            sender.sendMessage(prefix + ChatColor.GRAY + " ====== " + ChatColor.GREEN + name + ChatColor.GRAY + " ======");
+            for (Map.Entry<String, ICommand> entry : commands.entrySet()) {
+                if (!sender.hasPermission(permissions.get(entry.getKey())))
+                    continue;
+                sender.sendMessage(prefix + " " + ChatColor.YELLOW + "/" + label + " " + ChatColor.BOLD + entry.getKey() +
+                        ChatColor.YELLOW + " " + entry.getValue().arguments() + ChatColor.DARK_GRAY + " - " +
+                        ChatColor.GRAY + entry.getValue().help());
+            }
+            sender.sendMessage(" ");
+            return true;
+        } else if (args.length == 0) {
+            baseCommand.execute(sender, new String[0]);
+            return true;
+        } else {
+            if (!commands.containsKey(args[0])) {
+                sender.sendMessage(prefix + ChatColor.RED + " Unknown command.");
+                return true;
+            }
+            String[] subCommandArgs = Arrays.copyOfRange(args, label.split(" ").length, args.length);
+            commands.get(args[0]).execute(sender, subCommandArgs);
+            return true;
+        }
+    }
+}
